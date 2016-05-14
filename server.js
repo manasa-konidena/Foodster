ff8112a699159f192d358f5370009a006b6439c3
konidena12 <konidena12@ccs.neu.edu>
2016-05-14 18:09:17 -0400

    Home page edited

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
