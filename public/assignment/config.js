9b545153806ed4c182d0ef8025c60de194fbfcf5
konidena12 <konidena12@ccs.neu.edu>
2016-05-25 11:31:59 -0400

    Configured all routes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,15 +7,47 @@
     
     function Config($routeProvider) {
         $routeProvider
+            .when("/", {
+                templateUrl: "views/home.view.client.html"
+            })
             .when("/login", {
                 templateUrl: "views/user/login.view.client.html"
             })
             .when("/register", {
                 templateUrl: "views/user/register.view.client.html"
             })
-            .when("/profile", {
+            .when("/user/:uid", {
                 templateUrl: "views/user/profile.view.client.html"
             })
-
+            .when("/user/:uid/website", {
+                templateUrl: "views/website/website-list.view.client.html"
+            })
+            .when("/user/:uid/website/new", {
+                templateUrl: "views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "views/website/website-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "views/page/page-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "views/page/page-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateUrl: "views/page/page-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "views/widget/widget-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/widget/widget-choose.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "views/widget/widget-edit.view.client.html"
+            })
+            .otherwise({
+                redirectTo: "/login"
+            })
     }
 })();
