4dca0c741c8fe0ad384a90eb406b7b64fdf15f55
konidena12 <konidena12@ccs.neu.edu>
2016-06-01 22:04:34 -0400

    Started user service.

---
+++ assignment/services/user.service.server.js
@@ -0,0 +1,78 @@
+module.exports = function(app) {
+
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    app.get("/api/user", getUsers);
+    app.get("/api/user/:userId", findUserById);
+    app.delete("/api/user/:userId", deleteUser);
+
+    function deleteUser(req,res, newUser) {
+        var id = req.params.userId;
+        for(var i in users){
+            if(id === users[i]._id){
+                users.splice(i,1);
+                res.send(users);
+                return;
+            }
+        }
+        res.send(users);
+    }
+
+
+    function getUsers(req, res) {
+        var username = req.query.username;
+        var password = req.query.password;
+        if(username && password){
+            findUserByCredentials(username, password, res);
+        } else if(username){
+            findUserByUsername(username, res);
+        }else {
+            res.send(users);
+        }
+
+    }
+
+    function findUserByCredentials(username, password, res) {
+        for(var i in users){
+            if(users[i].username === username){
+                if(users[i].password === password){
+                    res.send(users[i]);
+                    return;
+                }else {
+                    //return "Pinc";
+                    res.send({});
+                    return;
+                }
+            }
+        }
+        // return "Unof";
+        res.send({});
+        return;
+    }
+
+    function findUserByUsername(username, res) {
+        for(var i in users){
+            if(username === users[i].username){
+                res.send(users[i]);
+                return;
+            }
+        }
+        res.send({});
+    }
+
+    function findUserById(req, res) {
+        var id = req.params.userId;
+        for(var i in users){
+                if(id === users[i]._id){
+                    res.send(users[i]);
+                    return;
+                }
+            }
+            res.send({});
+    }
+};
--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -3,12 +3,12 @@
         .module("WebAppMaker")
         .factory("UserService", UserService);
 
-    var users = [
-        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-    ];
+    // var users = [
+    //     {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+    //     {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+    //     {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+    //     {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    // ];
 
     function UserService() {
         var api = {
@@ -64,12 +64,7 @@
         }
 
         function findUserById(userId) {
-            for(var i in users){
-                if(users[i]._id === userId){
-                    return users[i];
-                }
-            }
-            return null;
+          
         }
 
         function findUserByUsername(username){
