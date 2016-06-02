8b3aa69f63b5b8d45bee1b5a320dc87196c72f08
konidena12 <konidena12@ccs.neu.edu>
2016-06-01 23:30:18 -0400

    Login Controller changed

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -3,14 +3,7 @@
         .module("WebAppMaker")
         .factory("UserService", UserService);
 
-    // var users = [
-    //     {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-    //     {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-    //     {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-    //     {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-    // ];
-
-    function UserService() {
+    function UserService($http) {
         var api = {
             createUser: createUser,
             findUserByCredentials: findUserByCredentials,
@@ -43,28 +36,12 @@
 
 
         function findUserByCredentials(username, password){
-           for(var i in users){
-               if(users[i].username === username){
-                   if(users[i].password === password){
-                       return users[i];
-                   }else {
-                       return "Pinc";
-                   }
-               }
-           }
-            return "Unof";
-
-            // for(var i in users){
-            //     if(users[i].username === username &&
-            //         users[i].password === password){
-            //         return users[i];
-            //     }
-            // }
-            // return null;
+            var url = "/api/user?username="+username+"&password="+password;
+            return $http.get(url);
         }
 
         function findUserById(userId) {
-          
+
         }
 
         function findUserByUsername(username){
--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -1,4 +1,4 @@
-(function () {
+(function(){
     angular
         .module("WebAppMaker")
         .controller("LoginController", LoginController);
@@ -11,37 +11,17 @@
 
         function login(username, password) {
 
-            var result = UserService.findUserByCredentials(username, password);
-
-            console.log(result);
-
-            if(result === "Unof"){
-                vm.error = "Username doesn't exist. Please Register.";
-            } else{
-                if(result === "Pinc"){
-                    vm.error = "Password is incorrect. Please try again!";
-                } else {
-                    $location.url("/user/"+result._id);
+            UserService
+                .findUserByCredentials(username, password)
+                .then(function(response){
+                console.log(response);
+                var user = response.data;
+                if(user){
+                    $location.url("/user/"+ user._id);
+                } else{
+                    vm.error = "User not found";
                 }
-            }
+            });
         }
-            //
-            // switch (user){
-            //     case "Pinc":
-            //         vm.error = "Password is incorrect. Please try again!";
-            //     case "Unof":
-            //         vm.error = "Username doesn't exist. Please register.";
-            //     default:
-            //         $location.url("/user/"+user._id);
-            // }
-        // 
-        //     if(user === "Pinc") {
-        //         vm.error = "Password is incorrect. Please try again!";
-        //     } if (user) {
-        //         $location.url("/user/"+user._id);
-        //     } else {
-        //         vm.error = "Oh SNAP! User not found";
-        //     }
-
     }
 })();
