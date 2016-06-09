4d780c3e947f5f3c3c823fefce8e70025c17b9ce
konidena12 <konidena12@ccs.neu.edu>
2016-06-08 23:34:09 -0400

    Assignment still works completly. Use when you mess up
    Trying project

--- public/project/config.js
+++ public/project/config.js
@@ -6,20 +6,42 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/", {
-                templateUrl: "views/home.view.client.html",
-
+                templateUrl: "views/home.view.client.html"
             })
             .when("/login", {
-                templateUrl: "views/user/login.view.client.html",
-
+                templateUrl: "views/user/prologin.view.client.html"
             })
             .when("/register", {
-                templateUrl: "views/user/register.view.client.html",
-
+                templateUrl: "views/user/proregister.view.client.html"
             })
-            .when("/profile", {
-                templateUrl: "views/user/profile.view.client.html",
+            // Changed routing for real purposes - changed it back
 
+            .when("/profile", {
+                templateUrl: "views/user/proprofile.view.client.html"
+            })
+            .when("/myrecipes", {
+                templateUrl: "views/recipe/myrecipes.view.client.html"
+            })
+            .when("/myphotos", {
+                templateUrl: "views/photo/myphotos.view.client.html"
+            })
+            .when("/followers", {
+                templateUrl: "views/user/followers.view.client.html"
+            })
+            .when("/following", {
+                templateUrl: "views/user/following.view.client.html"
+            })
+            .when("/favourites", {
+                templateUrl: "views/recipe/favourites.view.client.html"
+            })
+            .when("/welcomepage", {
+                templateUrl: "views/user/welcomepage.view.client.html"
+            })
+            .when("/cookedrecipes", {
+                templateUrl: "views/recipe/cookedrecipes.view.client.html"
+            })
+            .when("/personalinfo", {
+                templateUrl: "views/user/personalinfo.view.client.html"
             })
     }
 })();
